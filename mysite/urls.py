b7619f5189ef2f8eb2be73310746b4e3942207b9
brij1823 <brijpatel1823@gmail.com>
2020-01-18 19:03:36 +0530

    Second Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import author,interests,title
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',index),
+    path('author/',author),
+    path('interests/',interests),
+    path('title/',title),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 import json
 import scholarly
+import io
 
+import requests
 class Object:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -11,7 +13,8 @@ class Object:
 my_list = []
 json_list = []
 @api_view(['GET'])
-def index(request):
+def author(request):
+    try:
         temp = request.GET["data"]
         result = ((scholarly.search_author(temp)))
         json_content = []
@@ -27,4 +30,55 @@ def index(request):
             "url" : temp.url_picture
             }
             json_content.append(x)
-        return Response(json_content)
+    except:
+        print("Something Went Wrong")
+    return Response(json_content)
+
+@api_view(['GET'])
+def interests(request):
+    try:
+        temp = request.GET["data"]
+        result = scholarly.search_keyword(temp)
+        json_content = []
+        for i in range(1,20):
+            temp = next(result)
+            x = {
+            "id" : temp.id,
+            "name" : temp.name,
+            "affiliation" : temp.affiliation,
+            "citedby" : temp.citedby,
+            "email" : temp.email,
+            "interests" : temp.interests,
+            "url" : temp.url_picture
+            }
+            json_content.append(x)
+    except:
+        print("Something went wrong")
+    return Response(json_content)
+
+
+@api_view(['GET'])
+def title(request):
+    try:
+        temp = request.GET["data"]
+        result = scholarly.search_pubs_query(temp)
+        json_content = []
+        for i in range(1,10):
+            temp = next(result)
+            x = {
+            "title" : temp.bib["title"],
+            "author" : temp.bib["author"],
+            "abstract" : temp.bib["abstract"],
+            "citedby" : temp.citedby,
+            "id_scholarcitedby" : temp.id_scholarcitedby,
+            "source" : temp.source,
+            "url_scholar" : temp.url_scholarbib,
+            "E-Print" : temp.bib["eprint"]
+            }
+            json_content.append(x)
+            
+    except:
+        print("Something Went Wrong")
+    return Response(json_content)
+
+
